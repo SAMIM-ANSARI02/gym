ccef5063eab676ab9452b524b66e04ad6b1fa3e0
samim ansari <samimansari02@gmail.com>
2023-07-09 13:48:23 +0530

    modal part in registration

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 import { Link } from 'react-router-dom'
 import back4 from '../images/back4.jpg'
 import '../Styles/Home.css'
+import Mymodal from './Showmodal'
 
 const Home = () => {
+
+const[showmodal,setshowmodal]=useState(false);
+const Closemodal=()=>setshowmodal(false);
+
   return (
     <Layout>
       <div className='home' style={{backgroundImage:`url(${back4})`}} >
            <div className='container'>
            <h1>Your Fitness Our Priority </h1> 
            <p>Health is Wealth</p>
-            <Link to='/Registration'><button>join now</button></Link>
+            <button onClick={()=>setshowmodal(true)}>join now</button>
+            {showmodal && <Mymodal Closemodal={Closemodal} /> }
            
            </div>
       </div>
---
+++ src/Pages/Showmodal.js
@@ -0,0 +1,50 @@
+import React ,{ useEffect } from 'react'
+import '../Styles/Modal.css'
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
+import { Link } from 'react-router-dom';
+
+
+
+
+
+const Mymodal=({Closemodal})=>{
+    useEffect(()=>{
+        document.body.style.overflowY='hidden';
+        return()=>{document.body.style.overflowY='scroll'}
+    },[])
+    return(
+        <> 
+        <div className="modal_wrapper"></div>
+        <div className="modal_container">
+            <div style={{display:'flex'}}>
+            <h2 style={{fontSize:'50px',padding:'4px', textAlign:'center'}} >Welcome To Gym</h2>
+            <CloseIcon onClick={Closemodal} style={{color:'red', marginTop:'30px',marginLeft:'12px' ,fontSize:'30px'}}/>
+            </div>
+        
+        <p style={{fontSize:'20px',
+        color:'yellow',
+        // textAlign:'center',
+        display:'flex',
+        justifyContent:'flex-start'
+    }}><DoneIcon style={{color:'white'}}/>Done-for-you SEO service: SEO takes time and consistent updating to stay relevant. We’re the experts and we handle it for you. Always.</p>
+    <p style={{fontSize:'20px',
+        color:'yellow',
+        // textAlign:'center',
+        display:'flex',
+        justifyContent:'flex-start'
+    }}><DoneIcon style={{color:'white'}}/>Done-for-you SEO service: SEO takes time and consistent updating to stay relevant. We’re the experts and we handle it for you. Always.</p>
+    <p style={{fontSize:'20px',
+        color:'yellow',
+        // textAlign:'center',
+        display:'flex',
+        justifyContent:'flex-start'
+    }}><DoneIcon style={{color:'white'}}/>Done-for-you SEO service: SEO takes time and consistent updating to stay relevant. We’re the experts and we handle it for you. Always.</p>
+        <Link to='/registration'><button style={{width:'20% ',float:"left"}} className='modal_button'>registration</button></Link>
+        </div>
+       
+        </>
+       
+    )
+ }
+export default Mymodal
